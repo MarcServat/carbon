cae22eb9818ca927b12fe46b9ad402d150141f84
mkrds <mkordas@pgs-soft.com>
2021-12-08 17:22:40 +0100

    feat: enable tree shaking
    BREAKING CHANGE:
    To enable tree shaking modules have been changed from CommonJS to ES6.
    Before updating check if your environment supports ES6 modules

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,26 @@
-module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        useBuiltIns: "usage",
-        targets: {
-          browsers:
-            "last 2 Chrome versions, last 2 Firefox versions, last 2 Edge versions, last 2 Safari versions",
+module.exports = (api) => {
+  const isTest = api.env("test");
+
+  return {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          useBuiltIns: "usage",
+          targets: {
+            browsers:
+              "last 2 Chrome versions, last 2 Firefox versions, last 2 Edge versions, last 2 Safari versions",
+          },
+          corejs: "3.1",
+          modules: isTest ? "auto" : false,
         },
-        corejs: "3.1",
-      },
+      ],
+      "@babel/preset-react",
+      "@babel/preset-typescript",
     ],
-    "@babel/preset-react",
-    "@babel/preset-typescript",
-  ],
-  plugins: [
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-transform-classes",
-  ],
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-transform-classes",
+    ],
+  };
 };
